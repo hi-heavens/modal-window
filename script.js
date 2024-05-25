a2c5ed074089e7030fe4341a1587f4b8387d324a
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-05-25 07:37:56 +0100

    implemented the esc key feature

--- script.js
+++ script.js
@@ -20,5 +20,8 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 overlay.addEventListener('click', closeModal);
-
 btnCloseModal.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
