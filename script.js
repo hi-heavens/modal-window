cd33430b999cffdf2eee862f06bc820d50712057
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-05-25 07:04:55 +0100

    implement the close and open modal features

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+overlay.addEventListener('click', closeModal);
+
+btnCloseModal.addEventListener('click', closeModal);
